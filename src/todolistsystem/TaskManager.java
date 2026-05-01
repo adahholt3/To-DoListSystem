15c4e1aceb12f5e459e0a47c77e32b549b8d3b2e
aamnadham <aamna.dhamdachhawala@maine.edu>
2026-05-01 17:31:24 -0400

    Update TaskManager.java

--- src/todolistsystem/TaskManager.java
+++ src/todolistsystem/TaskManager.java
@@ -3,16 +3,16 @@ package todolistsystem;
  * Aamna D.
  */
 
-
 public class TaskManager {
 	
-	private MyArrayList<Task> tasks;
+	private MyArrayList<Task> tasks; // Setting MyArrayList for tasks
 	
-	public TaskManager() {
+	public TaskManager() { // Constructor
 		tasks = new MyArrayList<>();
 	}
-	// Create methods for adding,removing etc.
-	 
+	/**
+	*Create methods for adding,removing getting tasks, size of the tasks Myarraylist
+	*/ 
 	public void addTask (Task task) {
 		
 		tasks.add(task);
@@ -32,13 +32,17 @@ public class TaskManager {
 	 public int size() {
 		 return tasks.size();
 	 }
-	 
+	/**
+	* marking a task ad completed based on index 
+	*/ 
  	public void markTaskComplete(int index) {
 		 Task task = tasks.get(index);
 		 task.markComplete();
 	 }
 
-	 
+	/**
+	* displays (prints) all the tasks, and if there's no tasks a messege is displayed
+	*/ 
 	public void displayAllTasks() {
 		 if (tasks.size() == 0) {
 			 System.out.println("End of tasks, no tasks avalible");
@@ -50,8 +54,11 @@ public class TaskManager {
 	 }
 
 	
-	//filter by completion status
-	public void filterByCompletion(boolean completed) { // completion status, task type, priority
+	/**
+	* filter by task completions status by looping through the tasks
+	* prints tasks that are completed 
+	*/ 
+	public void filterByCompletion(boolean completed) { 
 		for (int i =0; i < tasks.size(); i++) {
 			Task t = tasks.get(i);
 			if (t.isCompleted() == completed) {
@@ -75,7 +82,7 @@ public class TaskManager {
 		
 	}
 	
-	//Fileter by priority which only works for priotiry tasks
+	//Filters by priority which only works for priority tasks
 	public void filterByPriority(int priority) {
 		
 		for(int i =0; i<tasks.size(); i++) {
