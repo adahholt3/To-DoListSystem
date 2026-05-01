f2c87ed6d35ba9ad20965fe8500eacd76f47f862
aamnadham <aamna.dhamdachhawala@maine.edu>
2026-05-01 17:18:38 -0400

    Update SimpleTask.java

--- src/todolistsystem/SimpleTask.java
+++ src/todolistsystem/SimpleTask.java
@@ -1,12 +1,13 @@
 package todolistsystem;
 /**
  * Aamna D.
+ * subclass of (task) parent class
  */
 import java.time.LocalDate;
 
 public class SimpleTask extends Task {
 
-	//Constructor for SimpleTask with the superClass attributes
+	//Constructor for SimpleTask with the superclass attributes
 	public SimpleTask(String title, String descriptions, boolean completed, LocalDate dueDate, int priority) {
 		super(title, descriptions, completed, dueDate, priority);
 	}
@@ -15,15 +16,12 @@ public class SimpleTask extends Task {
 	//Override methods from Task class 
 	@Override
 	public String getTaskType() {
-		return "SimpleTask";
-		
-		// Need to be fixed should return simple
-		// returns task type would probably be (recurring, simple, priority)
+		return "SimpleTask"; // returns simple task string
 	}
 
 	@Override
 	public String getDetails() {
-		return getDescriptions();
+		return getDescriptions(); // returns task details
 	
 
 	}
